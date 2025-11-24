1d37b63a573ea1ca7eed82447481f46dc8a9afe7
giovanni <giovanni6846@gmail.com>
2025-11-24 10:23:12 +0100

    Correction MAJ APP Mobile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,34 @@
-import { Stack } from 'expo-router';
-import { RootSiblingParent } from 'react-native-root-siblings';
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import * as Updates from "expo-updates";
+import { RootSiblingParent } from "react-native-root-siblings";
 
 export default function Layout() {
-   return (
-      <RootSiblingParent>
-         <Stack
-            screenOptions={{
-               headerShown: false,
-            }}
-         />
-      </RootSiblingParent>
-   );
+    const router = useRouter();
+
+    useEffect(() => {
+        async function checkOTA() {
+            try {
+                if (Updates.isEmbeddedLaunch) {
+                    const update = await Updates.checkForUpdateAsync();
+
+                    if (update.isAvailable) {
+                        await Updates.fetchUpdateAsync();
+                        await Updates.reloadAsync();
+                        return;
+                    }
+                }
+            } catch (error) {
+                console.log("Erreur pendant l'update OTA:", error);
+            }
+        }
+
+        checkOTA();
+    }, []);
+
+    return (
+        <RootSiblingParent>
+            <Stack screenOptions={{ headerShown: false }} />
+        </RootSiblingParent>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,14 @@
 import { useEffect } from "react";
-import { View, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
-import * as Updates from "expo-updates";
-import Toast from "react-native-root-toast";
+import { View, ActivityIndicator } from "react-native";
 
 export default function Index() {
     const router = useRouter();
 
     useEffect(() => {
-        async function checkOTA() {
-            try {
-                // Vérifie s'il y a une mise à jour
-                const update = await Updates.checkForUpdateAsync();
-
-                if (update.isAvailable) {
-                    // Télécharge la mise à jour
-                    await Updates.fetchUpdateAsync();
-                    // Recharge l'application sur la nouvelle version
-                    await Updates.reloadAsync();
-                    return;
-                }
-            } catch (error) {
-                console.log("Erreur pendant l'update OTA:", error);
-            }
-
-            Toast.show(`Route vers login`, {
-                duration: Toast.durations.SHORT,
-                position: Toast.positions.TOP,
-                backgroundColor: 'red', // vert
-                textColor: 'white',
-                shadow: true,
-                animation: true,
-                hideOnPress: true,
-                delay: 0,
-            });
-            router.push("/login");
-        }
-
-        checkOTA();
+        setTimeout(() => router.push("/login"), 10);
     }, []);
+
 
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
