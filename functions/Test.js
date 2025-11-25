e2c39d441f2afd0e7bcb14cc905b729bed37b8ca
giovanni <giovanni6846@gmail.com>
2025-11-25 12:57:13 +0100

    Correction MAJ APP Mobile

--- functions/Test.js
+++ functions/Test.js
@@ -3,26 +3,18 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export async function Test(){
 
-   /*const netInfo = await NetInfo.fetch();
-   if (!netInfo.isConnected) {
-      console.warn("📴 Hors ligne : pas de connexion Internet.");
-      return { ok: false, status: 'offline', message: "Vous êtes hors ligne." };
-   }*/
 
    try {
       const response = await fetch('http://10.6.0.7:28000/status', {
          method: 'GET',
          headers: { 'Content-Type': 'application/json' },
       });
-       console.log("STATUS:", response.status);
-
-       const data = await response.json();
-
-       console.log("DATA:", data);
+       console.log(">>> DEBUT FETCH");
 
       await AsyncStorage.setItem('connected', 'false');
       if (response.ok) {
          const data = await response.json();
+          console.log(">>> DATA:", data);
          await AsyncStorage.setItem('connected', 'true');
          return { ok: true, status: 'success', data };
       } else {
@@ -31,7 +23,10 @@ export async function Test(){
       }
 
    } catch (error) {
+       console.log(">>> FETCH ERROR:", error);
       await AsyncStorage.setItem('connected', 'false');
       return { ok: false, status: 'unreachable', message: "Serveur non joignable, veuillez réessayer plus tard." };
+
    }
+
 }
